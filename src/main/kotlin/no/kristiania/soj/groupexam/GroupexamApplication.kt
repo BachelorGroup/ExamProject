0cee059836dbff701b70eb1a5787f5c0a4d58fe5
Sebastian <seb.knutsen@gmail.com>
2018-10-17 11:42:52 +0200

    first commit setup project
    first commit of our amazing project in the subject PG6100

---
+++ src/main/kotlin/no/kristiania/soj/groupexam/GroupexamApplication.kt
@@ -0,0 +1,11 @@
+package no.kristiania.soj.groupexam
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GroupexamApplication
+
+fun main(args: Array<String>) {
+    runApplication<GroupexamApplication>(*args)
+}
