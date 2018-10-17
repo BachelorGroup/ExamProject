d3b6b98b2dda6650b0660598d0b3946440e6d61a
Jesper <jesper.dahl.ellingsen@hotmail.com>
2018-10-17 12:12:33 +0100

    Simple working REST functionality

---
+++ src/main/kotlin/no/kristiania/soj/groupexam/TestDto.kt
@@ -0,0 +1,9 @@
+package no.kristiania.soj.groupexam;
+
+import io.swagger.annotations.ApiModelProperty
+
+class TestDto {
+
+    @ApiModelProperty("The value to be checked")
+    var x: Int? = null
+}
