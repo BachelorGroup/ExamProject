5f89fb492bf3e8b9de04470cc11e33da0690493c
Ola Kveli <olakveli@yahoo.com>
2018-10-17 12:09:14 +0200

    actual update

--- src/main/kotlin/no/kristiania/soj/groupexam/GroupexamApplication.kt
+++ src/main/kotlin/no/kristiania/soj/groupexam/GroupexamApplication.kt
@@ -7,6 +7,5 @@ import org.springframework.boot.runApplication
 class GroupexamApplication
 
 fun main(args: Array<String>) {
-    println("ur gay lol")
     runApplication<GroupexamApplication>(*args)
 }
