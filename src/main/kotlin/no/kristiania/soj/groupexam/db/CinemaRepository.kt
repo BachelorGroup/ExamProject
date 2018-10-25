e3d157b487c0fc105db826bd383e2213e6247436
Ola Kveli <olakveli@yahoo.com>
2018-10-25 14:35:56 +0200

    Merge

--- src/main/kotlin/no/kristiania/soj/groupexam/db/CinemaRepository.kt
+++ src/main/kotlin/no/kristiania/soj/groupexam/db/CinemaRepository.kt
@@ -2,6 +2,7 @@ package no.kristiania.soj.groupexam.db
 
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import sun.security.krb5.internal.Ticket
 
 @Repository
 interface CinemaRepository : CrudRepository<Ticket, Long>
