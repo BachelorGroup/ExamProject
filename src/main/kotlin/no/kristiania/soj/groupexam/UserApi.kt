a747b24dc2b5384b9c1fbd483f492d24dd5db4d0
Sebastian <seb.knutsen@gmail.com>
2018-10-26 12:32:56 +0200

    added update to userapi

--- src/main/kotlin/no/kristiania/soj/groupexam/UserApi.kt
+++ src/main/kotlin/no/kristiania/soj/groupexam/UserApi.kt
@@ -7,11 +7,14 @@ import io.swagger.annotations.ApiResponse
 import no.kristiania.soj.groupexam.db.UserRepository
 import no.kristiania.soj.groupexam.dto.UserConverter
 import no.kristiania.soj.groupexam.dto.UserDTO
+import no.kristiania.soj.groupexam.util.RestResponseFactory
+import no.kristiania.soj.groupexam.util.WrappedResponse
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.validation.annotation.Validated
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.net.URI
 import javax.validation.ConstraintViolationException
 
 @RestController
@@ -27,71 +30,112 @@ class UserApi {
     @ApiOperation("Simple user creation")
     @PostMapping(consumes = [MediaType.APPLICATION_JSON_UTF8_VALUE])
     @ApiResponse(code = 201, message = "The username of the created user")
-    fun createTicket(
+    fun createUser(
             @ApiParam("Username and password. Do not specify role")
             @RequestBody
             dto: UserDTO
 
-    ): ResponseEntity<String> {
+    ): ResponseEntity<WrappedResponse<Void>> {
 
         if (!(dto.roles!!.isEmpty())) {
-            return ResponseEntity.status(400).build()
+            //for now all users created has the role 'USER'
+            return RestResponseFactory.userFailure("Cannot specify role when creating a user")
         }
 
         if (!dto.enabled!!) {
-            return ResponseEntity.status(400).build()
+            return RestResponseFactory.userFailure("User has to be enabled")
         }
 
         if (dto.username == null || dto.password == null) {
-            return ResponseEntity.status(400).build()
+            return RestResponseFactory.userFailure("Username or password is missing")
         }
 
         try {
             userCrud.createUser(dto.username!!, dto.password!!)
         } catch (e: Exception) {
             if(Throwables.getRootCause(e) is ConstraintViolationException) {
-                return ResponseEntity.status(400).build()
+                return RestResponseFactory.userFailure("Constraint violation")
             }
             throw e
         }
 
-        return ResponseEntity.status(201).body(dto.username)
+        return RestResponseFactory.created(URI.create("/api/users/" + dto.username))
     }
 
     @ApiOperation("Get all users")
     @GetMapping(path = ["/"])
-    fun get(
+    fun getAll(
 
-    ): ResponseEntity<List<UserDTO>> {
+    ): ResponseEntity<WrappedResponse<List<UserDTO>>> {
 
-        val list = userCrud.findAll()
-
-        return ResponseEntity.ok(UserConverter.transform(list))
+        return ResponseEntity.status(200).body(
+                WrappedResponse(
+                        code = 200,
+                        data = UserConverter.transform(userCrud.findAll()),
+                        message = "Fetched all users"
+                ).validated()
+        )
     }
 
     @ApiOperation("Get a single user specified by username")
     @GetMapping(path = ["/{username}"])
-    fun getUser(@ApiParam("The username of the user")
+    fun getByUsername(@ApiParam("The username of the user")
                 @PathVariable("username")
                 pathId: String?)
-            : ResponseEntity<UserDTO> {
+            : ResponseEntity<WrappedResponse<UserDTO>> {
 
-        val dto = userCrud.findById(pathId).orElse(null) ?: return ResponseEntity.status(404).build()
-        return ResponseEntity.ok(UserConverter.transform(dto))
+        val user = userCrud.findById(pathId).orElse(null)
+                ?: return RestResponseFactory.notFound(
+                "The requested user with username '$pathId' is not in the database")
+
+        return RestResponseFactory.payload(200, UserConverter.transform(user))
     }
 
     //private?
     @ApiOperation("Delete a user with the given username")
     @DeleteMapping(path = ["/{username}"])
-    fun delete(@ApiParam("The username of the user")
+    fun deleteByUsername(@ApiParam("The username of the user")
                @PathVariable("username")
-               pathId: String?): ResponseEntity<Any> {
+               pathId: String?): ResponseEntity<WrappedResponse<Void>> {
 
         if (!userCrud.existsById(pathId)) {
-            return ResponseEntity.status(404).build()
+            return RestResponseFactory.notFound(
+                    "The requested user with username '$pathId' is not in the database")
         }
 
         userCrud.deleteById(pathId)
-        return ResponseEntity.status(204).build()
+        return RestResponseFactory.noPayload(204)
+    }
+
+    @ApiOperation("Update a specific user")
+    @PutMapping(path = ["/{username}"], consumes = [MediaType.APPLICATION_JSON_UTF8_VALUE])
+    fun updateByUsername(
+            @ApiParam("The username of the user")
+            @PathVariable("username")
+            pathId: String,
+            //
+            @ApiParam("New data for updating the user")
+            @RequestBody
+            dto: UserDTO
+    ): ResponseEntity<WrappedResponse<Void>> {
+
+        if(dto.username == null){
+            return RestResponseFactory.userFailure("Missing username JSON payload")
+        }
+
+        if(dto.username != pathId){
+            return RestResponseFactory.userFailure("Inconsistent username between URL and JSON payload", 409)
+        }
+
+        val entity = userCrud.findById(pathId).orElse(null)
+                ?: return RestResponseFactory.notFound(
+                "The requested user with username '$pathId' is not in the database. " +
+                        "This PUT operation will not create it.")
+
+        entity.password = dto.password!!
+
+        userCrud.save(entity)
+
+        return RestResponseFactory.noPayload(204)
     }
 }
