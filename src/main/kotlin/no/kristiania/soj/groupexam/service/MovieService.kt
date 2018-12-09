e5d62bdc960df4eb0c2a5ed02f8a622c7935f1d9
Ola Kveli <olakveli@yahoo.com>
2018-12-09 13:51:22 +0100

    added basic services, need to expand more

---
+++ src/main/kotlin/no/kristiania/soj/groupexam/service/MovieService.kt
@@ -0,0 +1,56 @@
+package no.kristiania.soj.groupexam.service
+
+import no.kristiania.soj.groupexam.db.Movie
+import org.springframework.stereotype.Service
+import javax.persistence.EntityManager
+import javax.persistence.TypedQuery
+import javax.transaction.Transactional
+
+@Service
+@Transactional
+class MovieService(val entityManager: EntityManager) {
+
+    fun getMovie(id: Long): Movie? {
+        val movie = entityManager.find(Movie::class.java, id)
+        if (movie != null) {
+            return movie
+        }
+        return null
+    }
+
+    fun createMovie(title: String,
+                    director: String,
+                    description: String,
+                    info: String,
+                    rating: Int,
+                    releaseDate: String): Long? {
+        val movie = Movie(title = title,
+                director = director,
+                description = description,
+                info = info,
+                rating = rating,
+                releaseDate = releaseDate)
+        entityManager.persist(movie)
+
+        return movie.id
+    }
+
+    fun deleteMovie(movieID: Long) {
+        val movie = entityManager.find(Movie::class.java, movieID)
+        if (movie != null) {
+            entityManager.remove(movie)
+        }
+    }
+
+    fun getMoviesByDirector(director: String?): List<Movie> {
+        val query: TypedQuery<Movie>
+        if (director == null) {
+            query = entityManager.createQuery("select m from Movie m", Movie::class.java)
+        } else {
+            query = entityManager.createQuery("select m from Movie m where m.director=?1", Movie::class.java)
+            query.setParameter(1, director)
+        }
+        val result = query.resultList
+        return result
+    }
+}
---
+++ src/main/kotlin/no/kristiania/soj/groupexam/service/UserService.kt
@@ -0,0 +1,42 @@
+package no.kristiania.soj.groupexam.service
+
+import no.kristiania.soj.groupexam.db.User
+import org.springframework.stereotype.Service
+import javax.persistence.EntityManager
+import javax.persistence.TypedQuery
+import javax.transaction.Transactional
+
+@Service
+@Transactional
+class UserService(val entityManager: EntityManager) {
+    fun getUser(id: Long): User? {
+        val user = entityManager.find(User::class.java, id)
+
+        if (user != null) {
+            return user
+        }
+        return null
+    }
+
+    fun createUser(username: String, password: String, email: String): Long? {
+        val user = User(username = username, password = password, email = email)
+
+        entityManager.persist(user)
+
+        return user.id
+    }
+
+    fun deleteUser(userID: Long) {
+        val user = entityManager.find(User::class.java, userID)
+
+        if (user != null) {
+            entityManager.remove(user)
+        }
+    }
+    fun getUserByUsername(username: String?): User {
+        val query: TypedQuery<User> = entityManager.createQuery("select u from User u where u.username=?1", User::class.java)
+        query.setParameter(1, username)
+
+        return query.singleResult
+    }
+}
