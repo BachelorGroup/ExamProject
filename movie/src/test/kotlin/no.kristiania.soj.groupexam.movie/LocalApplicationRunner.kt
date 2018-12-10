915f3bb808af764965607be64dc9d53cc7c9c820
Ola Kveli <anonexam@yahoo.com>
2018-12-10 15:53:10 +0100

    Testing

---
+++ movie/src/test/kotlin/no.kristiania.soj.groupexam.movie/LocalApplicationRunner.kt
@@ -0,0 +1,7 @@
+package no.kristiania.soj.groupexam.movie
+
+import org.springframework.boot.SpringApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(GroupexamApplication::class.java, *args)
+}
