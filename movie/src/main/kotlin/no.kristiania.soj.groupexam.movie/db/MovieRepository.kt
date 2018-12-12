e03bbf2a7a13aa50cdb13c9b2886decf0528a58a
Ola Kveli <anonexam@yahoo.com>
2018-12-12 13:09:28 +0100

    refactoring and updates

--- movie/src/main/kotlin/no.kristiania.soj.groupexam.movie/db/MovieRepository.kt
+++ movie/src/main/kotlin/no.kristiania.soj.groupexam.movie/db/MovieRepository.kt
@@ -1,22 +1,13 @@
 package no.kristiania.soj.groupexam.movie.db
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import javax.persistence.EntityManager
 import javax.transaction.Transactional
 
 @Repository
 interface MovieRepository : CrudRepository<Movie, Long>, MovieRepositoryCustom {
-    fun findAllByMovie(movie: String): Iterable<Movie>
-
-    fun findAllByDirector(director: String): Iterable<Movie>
-
-    fun findAllByMovieAndDirector(movie: String, director: String): Iterable<Movie>
-
-    fun findAllByRating(rating: Int): Iterable<Movie>
-
-    fun findAllByDate(date: String): Iterable<Movie>
-
-    fun findAllById(id: Long): Iterable<Movie>
 }
 
 @Transactional
@@ -28,8 +19,6 @@ interface MovieRepositoryCustom {
                  info: String,
                  releaseDate: String): Long
 
-    fun updateInfo(id: Long, info: String): Boolean
-
     fun update(id: Long,
                title: String,
                director: String,
@@ -37,4 +26,29 @@ interface MovieRepositoryCustom {
                description: String,
                info: String,
                releaseDate: String): Boolean
+}
+@Repository
+@Transactional
+class MovieRepositoryImplementation : MovieRepositoryCustom {
+    @Autowired
+    private lateinit var entityManager: EntityManager
+
+    override fun addMovie(title: String, director: String, rating: Int, description: String, info: String, releaseDate: String): Long {
+       val movie = Movie(title, director, description, info, rating, releaseDate)
+        entityManager.persist(movie)
+
+        return movie.id!!
+    }
+
+    override fun update(id: Long, title: String, director: String, rating: Int, description: String, info: String, releaseDate: String): Boolean {
+        val movie = entityManager.find(Movie::class.java, id) ?: return false
+
+        movie.title = title
+        movie.director = director
+        movie.rating = rating
+        movie.description = description
+        movie.releaseDate = releaseDate
+
+        return true
+    }
 }
