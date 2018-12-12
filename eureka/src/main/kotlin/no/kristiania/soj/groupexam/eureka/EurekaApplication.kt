df3c9890fdf9110a6c3100c6e4e82301e91799f6
Sebastian <seb.knutsen@gmail.com>
2018-12-12 20:01:58 +0100

    oufbwoe

---
+++ eureka/src/main/kotlin/no/kristiania/soj/groupexam/eureka/EurekaApplication.kt
@@ -0,0 +1,14 @@
+package no.kristiania.soj.groupexam.eureka
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+
+@SpringBootApplication
+@EnableEurekaServer
+open class EurekaApplication
+
+
+fun main(args: Array<String>) {
+    SpringApplication.run(EurekaApplication::class.java, *args)
+}
