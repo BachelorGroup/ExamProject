96b085124bd7f411d561db16676ff7338d5e80dc
Ola Kveli <anonexam@yahoo.com>
2018-12-12 12:31:21 +0100

    pom re-writing

--- movie/src/test/kotlin/no.kristiania.soj.groupexam.movie/service/MovieServiceTest.kt
+++ movie/src/test/kotlin/no.kristiania.soj.groupexam.movie/service/MovieServiceTest.kt
@@ -1,6 +1,5 @@
 package no.kristiania.soj.groupexam.movie.service
 
-import no.kristiania.soj.groupexam.movie.db.Movie
 import org.jboss.arquillian.container.test.api.Deployment
 import org.jboss.arquillian.junit.Arquillian
 import org.jboss.shrinkwrap.api.ShrinkWrap
