c2f0c4ad28ffb1a501c803c6cde603aa742e51bd
Ola Kveli <olakveli@yahoo.com>
2018-12-13 16:16:40 +0100

    refactoring and removal

--- movie/src/main/kotlin/no.kristiania.soj.groupexam.movie/dto/HalObject.kt
+++
@@ -1,9 +0,0 @@
-package no.kristiania.soj.groupexam.movie.dto
-
-import io.swagger.annotations.ApiModelProperty
-
-open class HalObject(
-
-        @ApiModelProperty("HAL links")
-        var _links: MutableMap<String, HalLink> = mutableMapOf()
-)
