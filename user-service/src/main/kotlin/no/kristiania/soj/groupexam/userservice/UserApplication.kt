7bd45608fc843d414ea566e2b1c796b0704d612f
Jesper <jesper.dahl.ellingsen@hotmail.com>
2018-12-14 08:06:09 +0100

    Oof

--- user-service/src/main/kotlin/no/kristiania/soj/groupexam/userservice/UserApplication.kt
+++ user-service/src/main/kotlin/no/kristiania/soj/groupexam/userservice/UserApplication.kt
@@ -2,11 +2,26 @@ package no.kristiania.soj.groupexam.userservice
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
-@SpringBootApplication
-class UserApplication
+@SpringBootApplication(scanBasePackages = ["no.kristiania.soj.groupexam.userservice"])
+@EnableSwagger2
+@EnableDiscoveryClient
+class UserApplication {
 
-
+    @Bean
+    fun swaggerApi(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.any())
+                .build()
+    }
+}
 fun main(args: Array<String>) {
     SpringApplication.run(UserApplication::class.java, *args)
 }
